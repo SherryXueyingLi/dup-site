9af15e0de77ccfe64052e5e86412c441ad064098
Sherry Li <Sherry_Li@epam.com>
2016-05-23 15:38:50 +0800

    partical

--- scripts/cv/cv.js
+++ scripts/cv/cv.js
@@ -1,6 +1,9 @@
-define(["css!scripts/cv/cv"], function(){
+define(["particles","css!scripts/cv/cv"], function(particlesJS){
 
 	var controller = function($scope){
+		particlesJS.load('self-info', 'scripts/cv/particlesjs-config.json', function() {
+  			console.log('callback - particles.js config loaded');
+		});
 	};
 	
 	return controller;
